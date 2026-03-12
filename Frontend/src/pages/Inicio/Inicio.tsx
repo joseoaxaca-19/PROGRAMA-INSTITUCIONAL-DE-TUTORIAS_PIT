2ed7d486350b8dce506ee016b5e7a2e05c1996ef
Anapatricia11 <424139519@pcpuma.acatlan.unam.mx>
2026-03-11 23:21:45 -0600

    Add files via upload

--- Frontend/src/pages/Inicio/Inicio.tsx
+++ Frontend/src/pages/Inicio/Inicio.tsx
@@ -1,12 +1,51 @@
-//Aqui va a trabajar Ana
+import "./Inicio.css"
 
-//El siguiente codigo solo es para que node error cuando agreggues tu codigo eliminalo
-const Inicio = () => {
-  return (
-    <div>
-      <h1>Inicio</h1>
-    </div>
-  )
+import Navbar from "../../components/Navbar/Navbar.tsx"
+
+import fondo from "../../assets/images/Inicio.jpg"
+
+function Inicio(){
+
+return(
+
+<div>
+
+<Navbar/>
+
+<section
+className="hero"
+style={{ backgroundImage: `url(${fondo})` }}
+>
+
+<div className="hero-content">
+
+<h1>Programa de Tutorías</h1>
+
+<p>
+Impulsando tu éxito académico con el apoyo de nuestra comunidad
+universitaria y herramientas de aprendizaje colaborativo.
+</p>
+
+<div className="botones">
+
+<a href="#" className="btn-comenzar">
+Comencemos
+</a>
+
+<a href="#" className="btn-info">
+Ver más info
+</a>
+
+</div>
+
+</div>
+
+</section>
+
+</div>
+
+)
+
 }
 
 export default Inicio
