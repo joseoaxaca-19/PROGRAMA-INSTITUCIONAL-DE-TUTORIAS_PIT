422ea093e72704ba1e1f3f54c8b82dba5595a456
ohMaximoO <maxlugo_108@hotmail.com>
2026-03-16 21:21:00 -0600

    Backend con login y register

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const authRoutes = require("./routes/auth");
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Servidor funcionando 🚀");
+});
+
+app.listen(3000, () => {
+    console.log("Servidor corriendo en http://localhost:3000");
+});
