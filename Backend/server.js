edd00042ace689511e99c92bbd7506682e0866d9
CesarC-TG <320156825@pcpuma.acatlan.unam.mx>
2026-04-08 22:54:59 -0600

    Se agrego: citasController, modelo - citaModel, ruta - citas, base_de_datos - Tabla citas

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,11 @@ app.use(express.json());
 
 const authRoutes = require("./routes/auth");
 
+const citasRoutes = require("./routes/citas");
+
 app.use("/api/auth", authRoutes);
+
+app.use("/api/citas", citasRoutes);
 
 app.get("/", (req, res) => {
     res.send("Servidor funcionando 🚀");
