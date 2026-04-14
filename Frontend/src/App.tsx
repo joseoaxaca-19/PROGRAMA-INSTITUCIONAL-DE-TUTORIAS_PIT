b6e78bb597f8431041d0ac35d22b27aec390049e
ktrsca <ktrsca@gmail.com>
2026-04-13 20:38:53 -0600

    correccion de errores

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -6,6 +6,7 @@ import SobreNosotros from "./pages/SobreNosotros/SobreNosotros"
 import Servicios from "./pages/Servicios/Servicios"
 import Repositorio from "./pages/Repositorio/Repositorio"
 import RepositorioCarrera from "./pages/RepositorioCarrera/RepositorioCarrera"
+import Avisos from "./pages/Avisos"
 import SolicitarTutoria from "./pages/Servicios/SolicitarTutoria/SolicitarTutoria"
 
 
