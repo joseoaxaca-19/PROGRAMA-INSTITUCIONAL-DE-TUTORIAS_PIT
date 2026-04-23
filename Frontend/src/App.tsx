5bc4014d99afd8ed4b76f5090bdd98a9af6d90d8
ktrsca <ktrsca@gmail.com>
2026-04-22 21:21:18 -0600

    Agenda

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
-
 import Inicio from "./pages/Inicio/Inicio"
 import SobreNosotros from "./pages/SobreNosotros/SobreNosotros"
 import Servicios from "./pages/Servicios/Servicios"
@@ -11,46 +10,34 @@ import Avisos from "./pages/Avisos/Avisos"
 import AdminAvisos from "./pages/Avisos/AdminAvisos"
 import SolicitarTutoria from "./pages/Servicios/SolicitarTutoria/SolicitarTutoria"
 import Agenda from "./pages/Agenda/Agenda"
-
 import Bitacora from "./pages/Permisos/Bitacora/Bitacora"
-
+import Divisiones from "./pages/Divisiones/Divisiones"
+import Accesos from "./pages/Permisos/Accesos/Accesos"
+import Roles from "./pages/Permisos/Roles/Roles"
+import Usuarios from "./pages/Permisos/Usuarios/Usuarios"
 
 function App() {
-
   return (
-
     <BrowserRouter>
-
       <Routes>
-
         <Route path="/" element={<Inicio />} />
-
         <Route path="/sobre-nosotros" element={<SobreNosotros />} />
-
         <Route path="/servicios" element={<Servicios />} />
-
+        <Route path="/divisiones" element={<Divisiones />} />
         <Route path="/citas" element={<GestionCitas />} />
-
         <Route path="/repositorio" element={<Repositorio />} />
-
-        <Route path="/repositorio/:carrera" element={<RepositorioCarrera />} /> 
-
-        <Route path="/avisos" element={<Avisos />} /> 
-
-        <Route path="/admin-avisos" element={<AdminAvisos />} /> 
-
-        <Route path="/solicitar-tutoria" element={<SolicitarTutoria/>}/> 
-
+        <Route path="/repositorio/:carrera" element={<RepositorioCarrera />} />
+        <Route path="/avisos" element={<Avisos />} />
+        <Route path="/admin-avisos" element={<AdminAvisos />} />
+        <Route path="/solicitar-tutoria" element={<SolicitarTutoria />} />
         <Route path="/agenda" element={<Agenda />} />
-
         <Route path="/bitacora" element={<Bitacora />} />
-
+        <Route path="/accesos" element={<Accesos />} />
+        <Route path="/usuarios" element={<Usuarios />} />
+        <Route path="/roles" element={<Roles />} />
       </Routes>
-
     </BrowserRouter>
-
   )
-
 }
 
 export default App
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,43 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-
-import Inicio from './pages/Inicio/Inicio.tsx'
-import SobreNosotros from './pages/SobreNosotros/SobreNosotros.tsx'
-import Servicios from './pages/Servicios/Servicios.tsx'
-import Divisiones from './pages/Divisiones/Divisiones.tsx'
-import Avisos from './pages/Avisos/Avisos.tsx'
-import AdminAvisos from "./pages/Avisos/AdminAvisos.tsx"
-import Repositorio from './pages/Repositorio/Repositorio.tsx'
-import Login from './pages/Login/Login.tsx'
-import GestionCitas from './pages/Servicios/Citas/GestionCitas.tsx'
-
-import Bitacora from "./pages/Permisos/Bitacora/Bitacora.tsx"
-import Accesos from "./pages/Permisos/Accesos/Accesos.tsx"
-import Roles from "./pages/Permisos/Roles/Roles.tsx"
-import Usuarios from "./pages/Permisos/Usuarios/Usuarios.tsx"
-
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<Inicio />} />
-          <Route path="sobre-nosotros" element={<SobreNosotros />} />
-          <Route path="servicios" element={<Servicios />} />
-          <Route path="divisiones" element={<Divisiones />} />
-          <Route path="avisos" element={<Avisos />} />
-          <Route path="admin-avisos" element={<AdminAvisos />} />
-          <Route path="repositorio" element={<Repositorio />} />
-          <Route path="citas" element={<GestionCitas />} /> 
-
-          \\Permisos
-          <Route path="bitacora" element={<Bitacora />} />
-          <Route path="accesos" element={<Accesos />} />
-          <Route path="usuarios" element={<Usuarios />} />
-          <Route path="roles" element={<Roles />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
