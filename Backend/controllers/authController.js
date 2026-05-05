0bac8100641b0e361ace004db8721de8ccab8c2e
iman-blip <317140453@pcpuma.acatlan.unam.mx>
2026-05-05 10:58:10 -0600

    Update authController.js
    ← Cambiado de "./db" a "../config/db"

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,4 +1,4 @@
-const pool = require("./db");
+const pool = require("../config/db"); // ← Cambiado de "./db" a "../config/db"
 
 async function validarAcceso(usuarioId, carreraId) {
   const query = `
