3c17ed85624881c8f54b474b645f8f32b6ca9ca9
ktrsca <ktrsca@gmail.com>
2026-05-09 21:59:29 -0600

    correciones api

--- Frontend/src/services/api.ts
+++ Frontend/src/services/api.ts
@@ -1,4 +1,4 @@
-const API_URL = 'https://tu-backend.onrender.com/api';
+const API_URL = 'https://programa-institucional-de-tutorias-pit.onrender.com/api'
 
 const getHeaders = () => {
     const token = localStorage.getItem('token');
