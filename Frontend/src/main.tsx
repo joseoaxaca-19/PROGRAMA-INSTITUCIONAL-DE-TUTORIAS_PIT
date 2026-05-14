0468ce54fe35d81156fcee57057d685873bdd047
ktrsca <ktrsca@gmail.com>
2026-05-13 23:59:45 -0600

    Estandarizacion d estilos

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import { ThemeWrapper } from './components/ThemeWrapper'
+import './styles/variables.css'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
